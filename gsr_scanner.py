73fa863290a5f65a5b55e56dd59f7a432362fb3b
Mark42Lai <45850047+Mark42Lai@users.noreply.github.com>
2026-07-13 22:13:40 +0800

    Update gsr_scanner.py

--- gsr_scanner.py
+++ gsr_scanner.py
@@ -52,7 +52,7 @@ except UnicodeEncodeError as error:
 # =====================================================
 
 WINDOW = 12
-LOOKBACK_DAYS = 201
+LOOKBACK_DAYS = 450
 
 
 # =====================================================
